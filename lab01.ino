73c58c25ea495ab76974e4417fb1e9d47af69b82
04050582 <hc920052@gmail.com>
2017-03-29 21:16:45 +0800

    Add files via upload

---
+++ lab01.ino
@@ -0,0 +1,28 @@
+#include "SevSeg.h"
+ int Hour=0,Min=0,t=0;
+SevSeg sevseg;
+void setup() {
+byte numDigits = 4;
+byte digitPins[] = {5,4,3,2};
+byte segmentPins[] = {6, 7, 8, 9, 10, 11, 12, 13};
+sevseg.begin(COMMON_CATHODE, numDigits, digitPins,segmentPins);
+Serial.begin(9600);
+}
+void loop()
+{
+ 
+  sevseg.setNumber(Hour * 100 + Min, 4);
+  for(t=0;t<=30000;t++)
+  {
+    sevseg.refreshDisplay();
+  }
+Min = Min + 1;
+if(Min == 60)
+{ 
+Min = 0;
+if (Hour == 23) 
+Hour=0;
+else 
+Hour++;
+}
+}
